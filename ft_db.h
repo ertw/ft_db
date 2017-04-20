b6191649d51dfb54594361eeb902f69da3e44c74
rschramm <wobula@gmail.com>
2017-04-20 14:32:13 -0700

    Normed ft_db.h

--- ft_db.h
+++ ft_db.h
@@ -1,8 +1,22 @@
-#include "./libft/includes/libft.h"
-#include <fcntl.h>
-#include <termios.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_db.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rschramm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 15:55:00 by rschramm          #+#    #+#             */
+/*   Updated: 2017/01/13 18:10:43 by rschramm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct 		s_parse
+#ifndef FT_DB_H
+# define FT_DB_H
+# include "./libft/includes/libft.h"
+# include <fcntl.h>
+# include <termios.h>
+
+typedef struct		s_parse
 {
 	int				opt_l;
 	int				cert_present;
@@ -16,3 +30,5 @@ typedef struct		s_data
 
 int					ft_strdelic(char *s1, char *s2, char delim);
 void				user_login(t_parse *meta);
+
+#endif
