393477f7d2b32742cbe0753402890a242e8fa73d
rschramm <wobula@gmail.com>
2017-04-20 12:18:44 -0700

    Login system now prompts for user/pass if no cert if present

--- ft_db.c
+++ ft_db.c
@@ -22,7 +22,7 @@ int	cred_check(char *ptr, char *buff)
 	return (0);
 }
 
-int	handle_login(char **argv)
+int	handle_login(char *user, char *pass)
 {
 	int		fd;
 	int		r;
@@ -32,24 +32,106 @@ int	handle_login(char **argv)
 	fd = open("login.txt", O_RDONLY);
 	r = read(fd, &buff, 3999);
 	buff[r] = '\0';
-	ptr = ft_strjoin(argv[1], ">");
-	ptr = ft_strxjoin(ptr, argv[2], 1);
+	ptr = ft_strjoin(user, ">");
+	ptr = ft_strxjoin(ptr, pass, 1);
 	ptr = ft_strxjoin(ptr, "\n", 1);
 	if ((cred_check(ptr, buff)))
 		return (1);
 	return (0);
 }
 
-int		main(int argc, char **argv)
+int		login_certification(char *user, char *pass)
 {
-	if (argc < 3)
-		ft_putstr("usage: EZ-DB [login] [pw]\n");
+	int		x;
+	FILE	*fd;
+	time_t	current;
+
+	x = 0;
+	if ((handle_login(user, pass)) && (x = 1))
+	{
+		printf("Welcome to our database, %s.\n", user);
+		fd = fopen("cert.ers", "w+");
+		current = time(NULL);
+		fprintf(fd, "$%s>%s\n%s", user, pass, asctime(gmtime(&current)));
+	}
+	else
+		ft_putstr("Invalid username/Pass\n");
+	return (x);
+}
+
+void	cert_check(t_parse *user)
+{
+	if ((access("cert.ers", F_OK)) != -1)
+	{
+		user->cert_present = 1;
+		ft_putstr("Certificate Status: good\n");
+	}
 	else
 	{
-		if (handle_login(argv))
-			printf("Welcome to our database, %s.\n", argv[1]);
-		else
-			ft_putstr("Invalid username/Pass\n");
+		user->cert_present = 0;
+		ft_putstr("Certificate Status: meh\n");
 	}
+}
+
+void	parse_args(int argc, char **argv, t_parse *meta)
+{
+	int	c;
+
+	while ((c = getopt(argc, argv, "-l")) != -1)
+	{
+		if (c == 'l')
+			meta->opt_l = 1;
+		else
+		{
+			printf("EZ-DB: %c : invalid option\n", c);
+			ft_putstr("usage: EZ-DB [-l] [...]\n");
+			break ;
+		}
+	}
+	cert_check(meta);
+}
+
+char	*get_pass()
+{
+	int 	x;
+	char 	c;
+	char	pass[100];
+
+	x = 0;
+	printf("Enter password: ");
+	while ((scanf(" %c", &c) && c != '\n'))
+	{
+		pass[x] = c;
+		x++;
+	}
+	pass[x] = '\0';
+	return (ft_strdup(pass));
+}
+
+void	user_login(t_parse *meta)
+{
+	char	user[100];
+	char 	*pass;
+
+	printf("Enter username: ");
+	scanf(" %s", user);
+	pass = getpass("Enter pass: ");
+	if ((login_certification(user, pass)))
+		meta->cert_present = 1;
+	ft_strdel(&pass);
+}
+
+int		main(int argc, char **argv)
+{
+	t_parse meta;
+
+	if (argc == 1)
+		ft_putstr("usage: EZ-DB [-l] [...]\n");
+	else
+		parse_args(argc, argv, &meta);
+	if (meta.cert_present == 0)
+		user_login(&meta);
+	else
+		direct_to_main_prog(&meta);
 	return (0);
 }
--- ft_db.h
+++ ft_db.h
@@ -1,10 +1,17 @@
 #include "./libft/includes/libft.h"
 #include <fcntl.h>
+#include <termios.h>
+
+typedef struct 		s_parse
+{
+	int				opt_l;
+	int				cert_present;
+}					t_parse;
 
 typedef struct		s_data
 {
-	char		name[128];
-	int		age;
-}			t_data;
+	char			name[128];
+	int				age;
+}					t_data;
 
-int		ft_strdelic(char *s1, char *s2, char delim);
+int					ft_strdelic(char *s1, char *s2, char delim);
