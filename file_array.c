97b3b9608d5e3cf844881ca909bbd796c2eff374
Erik Williamson <erik@himself.in>
2017-04-20 03:46:51 +0000

    added debugging symbols to libft

--- file_array.c
+++ file_array.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #define NUM_ROWS 10
 #define ERR_NO_SPACE 10
+#define C_DATA(node289) ((t_data*)node289->content)
+
+typedef struct		s_data
+{
+	char		name[128];
+	int		age;
+}			t_data;
 
 /* database format:
  * line1 col1\n
@@ -10,37 +17,77 @@
  */
 
 /* takes a database FILE and a function that will do something with it */
-int		database_read(FILE *file, int (*action)(char *, int))
+t_list		*database_read(FILE *file)
 {
-	size_t		bufsize = 0;
-	char		*line;
-	char		**splitstring;
-	int		ret;
-	int		status;
+	t_data		data;
+	t_list		*node = NULL;
+	
+	if (fscanf(file, "%s %d", data.name, &data.age) == EOF)
+		return (NULL);
+	node = ft_lstnew(&data, sizeof(data));
+	node->next = database_read(file);
+	return (node);
+}
 
-	while ((status = getline(&line, &bufsize, file)) == 0)
+t_list			*find_name(t_list *alst, char *name)
+{
+	t_list		*node = NULL;
+	t_data		
+
+	if (!alst)
+		return (NULL);
+	if (!(strcmp(C_DATA(alst)->name, name)))
 	{
-		splitstring = ft_strsplit(line, ' ');
-		if (!(ret = action(splitstring[0], atoi(splitstring[1]))))
-		{
-			ft_strdel(&splitstring[1]);
-			ft_strdel(&splitstring[0]);
-			ft_memdel((void*)&splitstring);
-			return (ret);
-		}
+		node = ft_lstnew(C_DATA(alst), sizeof(t_data));
+		ft_lstmap();
+		node->next = find_name(alst->next, name);
 	}
-	return (0);
 }
 
-/* takes a person, returns an age */
-int		get_age(char *person, int age)
+/* print each node */
+void			lst_node_print(t_list *node)
 {
-
+	printf("%s %d\n", C_DATA(node)->name, C_DATA(node)->age);
 }
 
-/* takes a person and age, returns a success / failure */
-int		add_person(char *person, int age)
+/* delete each list node */
+void			lst_del_data(void *content, size_t sizeofcontent)
 {
-
+	if (!sizeofcontent)
+	{
+		;
+	}
+	ft_memdel((void**)(t_data*)&content);
 }
 
+int		main(void)
+{
+	t_list	*head;
+	t_list	*sorted;
+	t_list	*cursor;
+
+	if (!(head = database_read(fopen("file.db", "r"))))
+		return (1);
+	sorted = find_name(head, "ewilliam");
+	cursor = head;
+	while (cursor)
+	{
+		lst_node_print(cursor);
+		cursor = cursor->next;
+	}
+//	ft_lstiter(sorted, lst_node_print);
+	ft_lstdel(&head, lst_del_data);
+//	ft_lstdel(&sorted, lst_del_data);
+}
+
+///* takes a person, returns an age */
+//int		get_age(char *person, int age)
+//{
+//	if (!(strcmp()))
+//}
+
+///* takes a person and age, returns a success / failure */
+//int		add_person(char *person, int age)
+//{
+//
+//}
